5dbd0efdea8db7eb506545a3ed894b3b677db408
Mayeu <m@6x9.fr>
2013-07-24 03:02:25 +0200

    Replace comments by well-named methods

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,15 +1,32 @@
 class Bob
 
-   def hey (s)
-      case s
-      when nil,''       # Say nothing
+   def hey (sentence)
+      case
+      when match_say_nothing?(sentence)
          'Fine. Be that way.'
-      when s.upcase # Shouting
+      when match_yelling?(sentence)
          'Woah, chill out!'
-      when /\?$/     # Question
+      when match_asking?(sentence)
          'Sure.'
-      else          # Statement
+      when match_statement?(sentence)
          'Whatever.'
       end
    end
+
+   def match_say_nothing?(string)
+      !string || string == ''
+   end
+
+   def match_yelling?(string)
+      string == string.upcase
+   end
+
+   def match_asking?(string)
+      string.end_with? '?'
+   end
+
+   def match_statement?(string)
+      true
+   end
+
 end
