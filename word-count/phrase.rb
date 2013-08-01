2d748a9156bb2e433df703e04fc06e266f7fd777
Mayeu <m@6x9.fr>
2013-08-01 21:57:25 +0200

    Better methods names and variables names for phrase.rb

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -1,27 +1,26 @@
 class Phrase
   def initialize(phrase)
-    @phrase = phrase.to_s
-    normalize_phrase
-    @word_list = get_word_list
+    @phrase = normalize(phrase)
+    @words  = separate_words
   end
 
   def word_count
-    get_uniq_word.each_with_object({}) do |word, counted_word|
-      counted_word[word] = @word_list.count(word)
+    unique_words.each_with_object({}) do |word, count|
+      count[word] = @words.count(word)
     end
   end
 
   private
 
-  def normalize_phrase
-    @phrase.downcase!
+  def normalize(phrase)
+    phrase.to_s.downcase
   end
 
-  def get_word_list
+  def separate_words
     @phrase.scan(/\w+/)
   end
 
-  def get_uniq_word
-    @word_list.uniq
+  def unique_words
+    @words.uniq
   end
 end
